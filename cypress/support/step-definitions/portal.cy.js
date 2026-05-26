7c3b32d28b6858c301aab0606242be07b2a3ed25
Abreu-Renan <renan6basket@hotmail.com>
2026-05-26 08:18:17 -0300

    atualização

--- cypress/support/step-definitions/portal.cy.js
+++
@@ -1,9 +0,0 @@
-import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
-
-Given("que o usuário acesse a página de login", () => {
-  cy.visit('/');
-});
-
-And('clicar no botão "Signup"', () => {
-  cy.contains('Signup').click();
-});
